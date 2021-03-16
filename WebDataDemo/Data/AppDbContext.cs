2ab6b20cfbd0f9c57c8a82f4b8d3027b81934693
Steve Smith <steve@kentsmiths.com>
2021-03-16 11:59:59 -0400

    Initial version

---
+++ WebDataDemo/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebDataDemo.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ WebDataDemo/Model/Author.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace WebDataDemo.Model
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<CourseAuthor> Courses { get; set; } = new List<CourseAuthor>();
+    }
+
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+
+        public List<CourseAuthor> Authors { get; set; } = new List<CourseAuthor>();
+    }
+
+    public class CourseAuthor
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public int CourseId { get; set; }
+        public int RoyaltyPercentage { get; set; }
+    }
+}
